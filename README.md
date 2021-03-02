de6dc5b6cbe8c79650b638977057eb1769df76af
Léo <leo.delwaulle@gmail.com>
2021-03-02 12:11:10 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 2021-b5-ep1
+
+Equipe DATA + Réseau
+
+Léo, Romain, Edgar, Bryan, Alexandre
+
+Projet:
+Dashboard disponible via VM
+Tableau de suivi sur le covid / foot ou autre sujet
+Tableau intéractif avec plusieurs foncitonnalités
+
+Roles:
+l’équipe de développement:bryan, léo, edgar
+le Scrum master: Alexandre  
+le product owner:Romain
---
+++ app/weather.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import os
+import pytz
+import requests
+import math
+
+
+with open('api_key.txt', 'r') as file:
+    API_KEY = file.read().replace('\n', '')
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
+
+
+def query_api(city):
+    try:
+        print(API_URL.format(city, API_KEY))
+        data = requests.get(API_URL.format(city, API_KEY)).json()
+    except Exception as exc:
+        print(exc)
+        data = None
+    return data
